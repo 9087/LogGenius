8991c8c6ed6fe709d226e6bf94febe6321c4756a
Wu Zhiwei <503098961@QQ.com>
2025-09-29 00:32:30 +0800

    Add entry matching result cache.

--- LogGenius.Entries/EntryMatching.cs
+++ LogGenius.Entries/EntryMatching.cs
@@ -1,31 +1,56 @@
 ﻿using LogGenius.Core;
 using System;
+
 namespace LogGenius.Modules.Entries
 {
-    public static class EntryMatching
+    public class EntryMatchingMetaData
     {
-        public static bool Test(this Entry Entry, string FilterPattern, bool IsCaseSensitive, bool IsRegex)
+        public string? FilterPattern { get; set; }
+
+        public bool? IsCaseSensitive { get; set; }
+        
+        public bool? IsRegex { get; set; }
+
+        public bool? Result { get; set; }
+
+        public bool Test(Entry Entry, string FilterPattern, bool IsCaseSensitive, bool IsRegex)
         {
+            if (this.FilterPattern == FilterPattern && this.IsCaseSensitive == IsCaseSensitive && this.IsRegex == IsRegex)
+            {
+                return (bool) Result!;
+            }
+            this.FilterPattern = FilterPattern;
+            this.IsCaseSensitive = IsCaseSensitive;
+            this.IsRegex = IsRegex;
             if (string.IsNullOrEmpty(FilterPattern))
             {
-                return false;
+                Result = false;
             }
-            if (IsRegex)
+            else if (IsRegex)
             {
                 var Regex = RegexCache.Get(FilterPattern, IsCaseSensitive);
                 if (Regex == null)
                 {
-                    return false;
+                    Result = false;
                 }
                 else
                 {
-                    return Regex.IsMatch(Entry.Text);
+                    Result = Regex.IsMatch(Entry.Text);
                 }
             }
             else
             {
-                return Entry.Text.Contains(FilterPattern, IsCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase);
+                Result = Entry.Text.Contains(FilterPattern, IsCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase);
             }
+            return (bool)Result!;
+        }
+    }
+
+    public static class EntryMatching
+    {
+        public static bool Test(this Entry Entry, string FilterPattern, bool IsCaseSensitive, bool IsRegex)
+        {
+            return Entry.GetOrAddMetaData<EntryMatchingMetaData>().Test(Entry, FilterPattern, IsCaseSensitive, IsRegex);
         }
     }
 }
